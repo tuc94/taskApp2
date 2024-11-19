cb9c502fe96e509ab909b2455c6fbb23236a392f
tuc94 <tuc.baldecchi@gmail.com>
2024-11-19 16:45:02 -0500

    cleaning up branch

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const Task = require('./tasks')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -11,62 +11,70 @@ const userSchema = new mongoose.Schema({
         trim: true
     },
     email: {
-       type: String,
-       unique: true,
-       require: true,
-       trim: true,
-       lowercase: true,
-       validate(value){
-        if (!validator.isEmail(value)){
-            throw new Error('Email is invalid')
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
         }
-    }
     },
     password: {
         type: String,
-        require: true,
+        required: true,
+        minlength: 7,
         trim: true,
-        validate(value){
-            if (value.toLowerCase().includes("password")){
-                throw new Error('pasword is invalid')
-            }
-            if(!validator.isLength(value,6)){
-                throw new Error('password isnt long enough')
-            }
-        }
-     },
-    age:{
-        type: Number,
-        default: 0,
         validate(value) {
-            if (value < 0){
-                throw new Error('Age must be a positive number')
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
-    tokens:[{
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens: [{
         token: {
             type: String,
             required: true
         }
-    }]
+    }],
+    avatar: {
+        type: Buffer
+    }
 }, {
     timestamps: true
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.toJSON = function () {
-    const user = this 
+    const user = this
     const userObject = user.toObject()
 
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString()}, 'thisismynewcourse')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
@@ -75,23 +83,24 @@ userSchema.methods.generateAuthToken = async function () {
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(!user){
+    if (!user) {
         throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
 
-    if(!isMatch){
+    if (!isMatch) {
         throw new Error('Unable to login')
     }
 
-    return user 
+    return user
 }
 
-userSchema.pre('save', async function(next) {
-    const user = this 
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
 
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
@@ -100,21 +109,12 @@ userSchema.pre('save', async function(next) {
     next()
 })
 
-userSchema.virtual('tasks',{
-    ref: 'Task',
-    localField: '_id',
-    foreignField: 'owner'
-})
-
-
-// Delete user task when user is removed 
+// Delete user tasks when user is removed
 userSchema.pre('remove', async function (next) {
-    const user = this 
-    await Task.deleteMany({ owner: user._id}) 
-
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
-
 
 const User = mongoose.model('User', userSchema)
 
